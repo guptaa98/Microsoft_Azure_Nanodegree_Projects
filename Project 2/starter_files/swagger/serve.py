5f296a0891dd5579bb3d4b6a33d24039f8188919
Prashasti Gupta <prashasti708@gmail.com>
2021-01-11 18:51:12 +0000

    Add files via upload

--- Project 2/starter_files/swagger/serve.py
+++ Project 2/starter_files/swagger/serve.py
@@ -5,13 +5,13 @@ a swagger-ui service can pick it up from localhost.
 
 Run it with Python3:
 
-    $ python3 serve.py 8000
+    $ python3 serve.py 90
 
 The port number is optional, defaulting to 8000. Once the server is up and
 running with a swagger.json file in the same directory, then the url (assuming
 port 8000) to be used in swagger-ui would be:
 
-    http://localhost:8000/swagger.json
+    http://localhost:90/swagger.json
 
 """
 
@@ -35,6 +35,6 @@ if __name__ == "__main__":
         # Allows the port to be passed in as an argument
         port = sys.argv[-1]
     else:
-        port = 8000
+        port = 90
 
     test(CORSRequestHandler, HTTPServer, port=port)
